87f95cfeff218e3342c3c99f9ac53775d81e0304
DarkDeveloper-Plant <goldeagle00110011@outlook.com>
2021-05-23 07:15:27 +0430

    First Commit

---
+++ src/main/java/ir/darkdeveloper/anbarinoo/AnbarinooApplication.java
@@ -0,0 +1,13 @@
+package ir.darkdeveloper.anbarinoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnbarinooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnbarinooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/darkdeveloper/anbarinoo/AnbarinooApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.darkdeveloper.anbarinoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnbarinooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
