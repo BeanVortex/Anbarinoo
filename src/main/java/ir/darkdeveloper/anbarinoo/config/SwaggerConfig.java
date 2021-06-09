eb33997eb1dbb9a3160dc6004c8c6885988d415f
DarkDeveloper-Plant <goldeagle00110011@outlook.com>
2021-06-09 13:30:18 +0430

    00:03:19
    - Configured with refresh token but stil is not usable

--- src/main/java/ir/darkdeveloper/anbarinoo/config/SwaggerConfig.java
+++ src/main/java/ir/darkdeveloper/anbarinoo/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package ir.darkdeveloper.anbarinoo.config;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,16 +27,41 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerConfiguration(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey("refresh","refresh_token"), apiKey("access","access_token")))
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
                 .apis(RequestHandlerSelectors.basePackage("ir.darkdeveloper"))
-                .build()
-                .apiInfo(apiDetails());
+                .build();
     }
 
-    private ApiInfo apiDetails(){
-        return new ApiInfo("fds", "hggf", "1.0", "free", new Contact("dsaf", "null", "null"), "dsfa", ":dsf.com", Collections.emptyList());
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "REST API OVERVIEW",
+          "apis are only for overview in swagger",
+          "1.0",
+          "Terms of service",
+          new Contact("DarkDeveloper", "", "darkdeveloper@outlook.com"),
+          "License of API",
+          "API license URL",
+          Collections.emptyList());
     }
  
+    private ApiKey apiKey(String key, String name) { 
+        return new ApiKey(key, name, "header"); 
+    }
+
+    private SecurityContext securityContext() { 
+        return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+    } 
+    
+    private List<SecurityReference> defaultAuth() { 
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[2]; 
+        authorizationScopes[0] = authorizationScope; 
+        authorizationScopes[1] = authorizationScope; 
+        return Arrays.asList(new SecurityReference("refresh", authorizationScopes), new SecurityReference("access", authorizationScopes)); 
+    }
 
 }
