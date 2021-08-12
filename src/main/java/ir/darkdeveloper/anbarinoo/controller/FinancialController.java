c040c13cc622e0de2b2a3472f5f7f04933569596
DarkDeveloper-Plant <goldeagle00110011@outlook.com>
2021-08-13 00:53:22 +0430

    00:05:22
    - Added financial Controller

--- src/main/java/ir/darkdeveloper/anbarinoo/controller/FinancialController.java
+++ src/main/java/ir/darkdeveloper/anbarinoo/controller/FinancialController.java
@@ -1,9 +1,12 @@
 package ir.darkdeveloper.anbarinoo.controller;
 
+import ir.darkdeveloper.anbarinoo.model.FinancialModel;
 import ir.darkdeveloper.anbarinoo.service.FinancialService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/user/financial")
@@ -15,4 +18,22 @@ public class FinancialController {
     public FinancialController(FinancialService service) {
         this.service = service;
     }
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveFinancial(@RequestBody FinancialModel financial, HttpServletRequest req) {
+        return ResponseEntity.ok(service.saveFinancial(financial, req));
+    }
+
+    @PutMapping("/{id}/")
+    public ResponseEntity<?> updateFinancialById(@RequestBody FinancialModel financial, @PathVariable Long id,
+                                                 HttpServletRequest req) {
+        return ResponseEntity.ok(service.updateFinancial(financial, id, req));
+    }
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<?> getFinancialById(@PathVariable Long id, HttpServletRequest req) {
+        return ResponseEntity.ok(service.getFinancial(id, req));
+    }
+
+
 }
