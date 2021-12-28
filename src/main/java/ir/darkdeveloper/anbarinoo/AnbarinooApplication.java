dbe010d4553567a70c0d6368bbe8e22b148bac00
darkdeveloper <goldeagle00110011@outlook.com>
2021-12-28 22:56:28 +0330

    00:10:07
    - Maybe secure against log4j vul

--- src/main/java/ir/darkdeveloper/anbarinoo/AnbarinooApplication.java
+++ src/main/java/ir/darkdeveloper/anbarinoo/AnbarinooApplication.java
@@ -1,16 +1,20 @@
 package ir.darkdeveloper.anbarinoo;
 
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@Log4j2
 public class AnbarinooApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AnbarinooApplication.class, args);
+        log.error("d");
     }
 
     @Override
